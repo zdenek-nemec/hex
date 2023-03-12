975160b67196152ed3e8c0efae25d9c2768c5090
zdenek-nemec <zdenek.nemec@artin.cz>
2023-03-12 23:59:12 +0100

    #2: Number conversion template

--- website/auth.py
+++ website/auth.py
@@ -1,10 +1,13 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request
 
 auth = Blueprint("auth", __name__)
 
 
-@auth.route("/hex")
+@auth.route("/hex", methods=["GET", "POST"])
 def hex():
+    if request.method == "POST":
+        number = request.form.get("number")
+        return render_template("hex.html", submitted=True, number=number)
     return render_template("hex.html")
 
 
