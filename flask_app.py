e57c397dc47b99ce4614a7c765e26422bbb2d1d2
Zdeněk Němec <Zdenek.Nemec@ysoft.com>
2025-09-30 16:18:01 +0200

    #20 Changed `flask_app.py` to be able to deploy on PA

--- flask_app.py
+++ flask_app.py
@@ -1,4 +1,10 @@
-from main import main
+from website import create_application
+
+app = create_application()
 
 if __name__ == "__main__":
-    main()
+    # argument_parser = argparse.ArgumentParser()
+    # argument_parser.add_argument("--debug", action="store_true")
+    # application = create_application()
+    # application.run(debug=argument_parser.parse_args().debug)
+    app.run(debug=False)
