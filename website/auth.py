461996300fd245f584db5162478e40add082ae3c
zdenek-nemec <zdenek.nemec@artin.cz>
2025-11-25 22:25:58 +0100

    #3 Added numbers form and updated CSS for precedence

--- website/auth.py
+++ website/auth.py
@@ -11,8 +11,16 @@ def home():
     return render_template("home.html")
 
 
-@auth.route("/numbers")
+@auth.route("/numbers", methods=["GET", "POST"])
 def numbers():
+    if request.method == "POST":
+        number = request.form.get("number")
+        try:
+            output = [f"Original (decimal): {number}"]
+        except:
+            flash("Error", category="error")
+            output = []
+        return render_template("numbers.html", content=output)
     return render_template("numbers.html")
 
 
