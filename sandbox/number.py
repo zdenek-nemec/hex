03e25d672f55b5cb2bbef3c09c41f751bbac0914
zdenek-nemec <zdenek.nemec@artin.cz>
2025-11-26 21:41:53 +0100

    #3 Number-base validation

--- sandbox/number.py
+++ sandbox/number.py
@@ -1,7 +1,7 @@
 class Number(object):
     def __init__(self, value: str, base: int = 10):
         super(Number, self).__init__()
-        # self._validate(value, base)  # TODO: Validate that the string value could be a number in the given base
+        self._validate(value, base)
         self._str_value = value
         self._base = base
         self._int_value = int(value, base)
@@ -9,8 +9,16 @@ class Number(object):
     def __str__(self):
         return f"{self._str_value} base {self._base}"
 
+    @staticmethod
+    def _validate(value: str, base: int):
+        if base < 2 or base > 16:
+            raise ValueError("Base must be between 2 and 16")
+        allowed_characters = "0123456789ABCDEF"[:base]
+        if any(c.upper() not in allowed_characters for c in value):
+            raise ValueError(f"Value {value} is not valid for base {base}")
+
     def get(self, base: int = 10):
-        return str(self._int_value)  # TODO
+        return str(self._int_value)  # TODO: Implement base conversion
 
 
 def main():
--- tests/unit_tests/test_number.py
+++ tests/unit_tests/test_number.py
@@ -12,6 +12,20 @@ class TestNumber(unittest.TestCase):
         self.assertEqual(str(my_number), "42 base 10")
         self.assertEqual(my_number.get(), "42")
 
+    def test_number_binary(self):
+        my_number = Number("1001", 2)
+        self.assertEqual(str(my_number), "1001 base 2")
+        self.assertEqual(my_number.get(), "9")
+
+    def test_exception_invalid_base(self):
+        self.assertRaises(ValueError, Number, "0000", 1)
+        self.assertRaises(ValueError, Number, "0000", 17)
+
+    def test_exception_invalid_number_for_base(self):
+        self.assertRaises(ValueError, Number, "0123", 2)
+        self.assertRaises(ValueError, Number, "10A", 10)
+        self.assertRaises(ValueError, Number, "EF", 8)
+
 # TODO
 # Test with different bases (e.g., binary, octal, hexadecimal).
 # Test with invalid base values (e.g., negative, zero, or unsupported bases).
